2a6b1d2da0aa143dbff5d9e1fe802ce4dee99b4d
Abraão <abraaomauro.silva@gmail.com>
2026-09-21 09:16:48 -0400

    Remove caminho antigo WACAD008 após renomeação

--- WACAD008/Laboratorio03/src/interfaces.ts
+++
@@ -1,8 +0,0 @@
-export interface Produto {
-  readonly id: number;
-  readonly modelo: string;
-  readonly fabricante: string;
-  readonly valor: number;
-  getTipo(): string;
-  getDetalhes(): string;
-}
